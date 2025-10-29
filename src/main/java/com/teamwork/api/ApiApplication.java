f6b0b3d4422462b8d65261bfabfe40eeafa19c7c
Zenden1982 <nikita_putilin@bk.ru>
2025-10-29 19:33:25 +0300

    docker

--- src/main/java/com/teamwork/api/ApiApplication.java
+++ src/main/java/com/teamwork/api/ApiApplication.java
@@ -2,10 +2,8 @@ package com.teamwork.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile("h2")
 public class ApiApplication {
 
 	public static void main(String[] args) {
