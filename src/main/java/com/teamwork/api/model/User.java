38384b6d30e18a192f17bddb4fae9b6ae7dae5db
Zenden1982 <nikita_putilin@bk.ru>
2025-11-02 12:51:04 +0300

    update

--- src/main/java/com/teamwork/api/model/User.java
+++ src/main/java/com/teamwork/api/model/User.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -59,13 +60,12 @@ public class User implements UserDetails {
 
     private Boolean active;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Order> orders;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Review> reviews;
 
-    
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles.stream()
